39008e499e20e633a552f1ec1154ae35e48fcdd6
VictorCam <43551031+VictorCam@users.noreply.github.com>
2020-08-12 23:29:54 -0700

    poop

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+const serveStatic = require("serve-static")
 
 app.use(cors());
 app.use(bodyParser.json());
